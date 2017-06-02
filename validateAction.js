0bb0767462a50a3b6c97756ae8cdb1e5250c23e4
Garima Kamboj <garima.kamboj@icloud.com>
2017-06-02 11:32:23 +0530

    Add validations for the action

---
+++ validateAction.js
@@ -0,0 +1,12 @@
+const validateAction = action => {
+  if (!action || typeof action !== 'object' || Array.isArray(action)) {
+    throw new Error('Action must be an object!'); 
+  }
+  if (typeof action.type === 'undefined') {
+    throw new Error('Action must have a type!');
+  }
+}
+
+console.log(validateAction());
+
+module.exports = validateAction;
